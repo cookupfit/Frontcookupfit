e0ed1b4ba011d984c2187c2b0b2208fe7f52ff25
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-12-28 03:17:15 +0100

    make buttons on test page take you to other pages

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,13 +1,83 @@
 // In Button.js
 import React from 'react';
-import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { Button, View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
-
-const Test = ({ onPress, title }) => {
+const Test = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text>Test Screen</Text>
+
+      {/* single buttons */}
+      <Button
+        title="Login"
+        onPress={() => navigation.navigate('Login')}
+      />
+      <Button
+        title="Main"
+        onPress={() => navigation.navigate('Main')}
+      />
+      <Button
+        title="Profile"
+        onPress={() => navigation.navigate('Profile')}
+      />
+      <Button
+        title="Settings"
+        onPress={() => navigation.navigate('Settings')}
+      />
+
+      {/* managment buttons */}
+      <Button
+        title="Createpaiment"
+        onPress={() => navigation.navigate('Createpaiment')}
+      />
+      <Button
+        title="Createuser"
+        onPress={() => navigation.navigate('Createuser')}
+      />
+      <Button
+        title="Dtailuserpaiment"
+        onPress={() => navigation.navigate('Dtailuserpaiment')}
+      />
+      <Button
+        title="Edituser"
+        onPress={() => navigation.navigate('Edituser')}
+      />
+      <Button
+        title="Mainmanagment"
+        onPress={() => navigation.navigate('Mainmanagment')}
+      />
+
+      {/* plans buttons */}
+      <Button
+        title="Createplan"
+        onPress={() => navigation.navigate('Createplan')}
+      />
+      <Button
+        title="Editplan"
+        onPress={() => navigation.navigate('Editplan')}
+      />
+      <Button
+        title="Listplan"
+        onPress={() => navigation.navigate('Listplan')}
+      />
+
+      {/* program buttons */}
+      <Button
+        title="Createprogram"
+        onPress={() => navigation.navigate('Createprogram')}
+      />
+      <Button
+        title="Editprogram"
+        onPress={() => navigation.navigate('Editprogram')}
+      />
+      <Button
+        title="Listprogram"
+        onPress={() => navigation.navigate('Listprogram')}
+      />
+
     </View>
     
   );
