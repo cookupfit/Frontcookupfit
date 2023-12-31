caff0498750352a65c5750b4dec449bd78880383
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-12-31 12:21:26 +0100

    i make the border and circle

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import PieChart from 'react-native-pie-chart';
+import { FONT_REGULAR, greenColor } from '../assets/colors';
 
 const Main = () => {
   const widthAndHeight = 200;
   const series = [75, 25];
   const sliceColor = ['rgb(255, 99, 132)', 'rgb(255, 205, 86)'];
+  const widthAndHeightred = 10;
+  const widthAndHeightyellow = 10;
+  const red = [100, 0];
+  const yellow = [0, 100];
 
   return (
     <View style={styles.container}>
       <View style={styles.chartContainer}>
         <View style={styles.data}>
-          <Text style={styles.label}>Pending</Text>
-          <Text style={styles.label}>Required</Text>
+          <View style={styles.pay}>
+            <PieChart style={styles.pieChart} widthAndHeight={widthAndHeightred} series={red} sliceColor={sliceColor} />
+            <Text style={styles.label}>Payment Pending</Text>
+          </View>
+          <View style={[styles.pay, { marginTop: 30 }]}>
+            <PieChart style={styles.pieChart} widthAndHeight={widthAndHeightyellow} series={yellow} sliceColor={sliceColor} />
+            <Text style={styles.label}>Payment Pending</Text>
+          </View>
         </View>
         <PieChart widthAndHeight={widthAndHeight} series={series} sliceColor={sliceColor} />
       </View>
@@ -26,20 +37,40 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'rgba(10, 14, 17, 1)',
   },
-  label: {
-    color: 'white',
-    fontFamily: 'FONT_REGULAR', // Make sure this font family is defined
-    fontSize: 20,
-  },
+  
   chartContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
-    width: '100%', // Adjust the width as needed
+    width: '95%',
+    height: '33%',
+    borderWidth: 2,
+    borderColor: greenColor,
+    borderRadius: 45,
+
+  },
+  label: {
+    color: 'white',
+    fontFamily: FONT_REGULAR,
+    fontSize: 15,
+    marginLeft: 15, // Adjust the marginLeft value as needed
   },
   data: {
-    flexDirection: 'column',
+    flexDirection: 'column', // Change to row to place PieCharts and Texts side by side
     alignItems: 'center',
+    justifyContent: 'space-around',
+    
+  },
+  pay: {
+    flexDirection: 'row', // Change to row to place PieChart and Text side by side
+    alignItems: 'center',
+    marginRight: 10, // Adjust the marginRight value as needed
+    
+  },
+  
+  pieChart: {
+    marginRight: 5, // Adjust the marginRight value as needed
+    
   },
 });
 
