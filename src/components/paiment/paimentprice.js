f5ddbc02c3a7e51782e0889758b39964cfc84069
adnanebensouda <bensouda.adnane.dev@gmail.com>
2024-01-14 22:45:26 +0100

    i just make input price and price checkbox

---
+++ src/components/paiment/paimentprice.js
@@ -0,0 +1,97 @@
+import React, {useState, useEffect, useRef} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
+  Animated,
+  Easing,
+  ScrollView,
+  Touchable,
+  TouchableOpacity,
+} from 'react-native';
+import {
+  redColor,
+  FONT_BOLD,
+  yellowColor,
+  greenColor,
+  greyColor,
+} from '../../assets/colors';
+
+
+const styles = StyleSheet.create({
+  
+});
+
+const Paimentprice = () => {
+    const [isChecked, setChecked] = useState(false);
+
+  const toggleCheckBox = () => {
+    setChecked(!isChecked);
+  };
+
+  
+  return (
+    <View style={{
+        width: '90%',
+      }}>
+              <View
+                style={{
+                  height: 70,
+                  borderWidth: 1,
+                  borderRadius: 15,
+                  fontFamily: FONT_BOLD,
+                  fontSize: 30,
+                  borderWidth: 4,
+                  borderColor: greyColor,
+                  
+                }}>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    
+                    alignItems: 'center',
+                    justifyContent: 'space-around',
+                    height: '100%',
+                    
+                  }}>
+                  <Text style={{color: greyColor,
+        fontFamily: FONT_BOLD,
+        fontSize: 25,}}>06/23</Text>
+                  <Text style={{color: greyColor,
+        fontFamily: FONT_BOLD,
+        fontSize: 25,}}>200</Text>
+                  <TouchableOpacity
+                    style={{
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                    }}
+                    onPress={toggleCheckBox}>
+                    <View
+                      style={{
+                        
+                          width: 20,
+                          height: 20,
+                          marginRight: 8,
+                          borderWidth: 2,
+                          borderColor: greyColor,
+                          borderRadius: 5,
+                        
+                        
+                          backgroundColor: isChecked
+                            ? redColor
+                            : 'transparent',
+                        
+                      }}
+                    />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+  );
+};
+
+export default Paimentprice;
--- src/pages/managment/Createpaiment.js
+++ src/pages/managment/Createpaiment.js
@@ -1,7 +1,28 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
-import { redColor, FONT_BOLD, blackColor } from '../../assets/colors';
+import React, {useState, useEffect, useRef} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
+  Animated,
+  Easing,
+  ScrollView,
+  Touchable,
+  TouchableOpacity,
+} from 'react-native';
+import {
+  redColor,
+  FONT_BOLD,
+  yellowColor,
+  greenColor,
+  greyColor,
+} from '../../assets/colors';
 
+
+import Paimentprice from '../../components/paiment/paimentprice';
 // notification component
 import Notification from '../../components/notifaction/notification';
 import Userdata from '../../components/datacharts/userdata';
@@ -23,72 +44,107 @@ const styles = StyleSheet.create({
   },
   profileview: {
     width: '100%',
-    flex: 0.9,
     alignItems: 'center',
     justifyContent: 'center',
   },
   listpaiement: {
-    backgroundColor: 'purple',
+    backgroundColor: greenColor,
     width: '100%',
     flex: 1.6,
     borderTopLeftRadius: 50,
     borderTopRightRadius: 50,
     justifyContent: 'center',
-  },
-  text: {
-    fontSize: 20,
-    fontFamily: FONT_BOLD,
-    color: blackColor,
-  },
-  // checkbox
-  checkBoxContainer: {
-    flexDirection: 'row',
     alignItems: 'center',
+  },
+  input: {
+    height: 70,
+    borderWidth: 1,
+    borderRadius: 15,
+    paddingRight: 9,
+    paddingLeft: 25,
+    fontFamily: FONT_BOLD,
+    fontSize: 30,
+    borderWidth: 4,
+    borderColor: greyColor,
+  },
+  v_view: {
+    width: '90%',
   },
   checkBox: {
     width: 20,
     height: 20,
     marginRight: 8,
-    borderWidth: 1,
-    borderColor: 'white', // You can change the border color as needed
+    borderWidth: 2,
+    borderColor: greyColor,
     borderRadius: 5,
   },
 });
 
 const Createpaiment = () => {
-  const [isChecked, setChecked] = useState(false);
+  const [number, onChangeNumber] = React.useState('');
 
-  const toggleCheckBox = () => {
-    setChecked(!isChecked);
-  };
+  const [keyboardVisible, setKeyboardVisible] = useState(false);
+  const flexAnim = useRef(new Animated.Value(0.9)).current;
 
-  const [text, onChangeText] = React.useState('Useless Text');
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      () => {
+        Animated.timing(flexAnim, {
+          toValue: 0,
+          duration: 50, // Adjust the duration for a smoother effect
+          easing: Easing.easeInOut, // Use "ease-in-out" easing function
+          useNativeDriver: false,
+        }).start();
+        setKeyboardVisible(true);
+      },
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      () => {
+        Animated.timing(flexAnim, {
+          toValue: 0.9,
+          duration: 1000,
+          easing: Easing.easeInOut,
+          useNativeDriver: false,
+        }).start();
+        setKeyboardVisible(false);
+      },
+    );
+
+    return () => {
+      keyboardDidHideListener.remove();
+      keyboardDidShowListener.remove();
+    };
+  }, [flexAnim]);
+
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={styles.container}>
       <Notification />
-      <View style={styles.profileview}>
-        <Userdata month="03" year="23" />
-      </View>
-      <View style={styles.listpaiement}>
-        {/*
-        <TouchableOpacity
-          style={styles.checkBoxContainer}
-          onPress={toggleCheckBox}
-        >
-          <View style={[styles.checkBox, { backgroundColor: isChecked ? 'rgba(45, 231, 44, 1)' : 'transparent' }]} />
-        </TouchableOpacity>
-        */}
-        
-      <TextInput
-        style={{borderWidth: 1, borderRadius: 10}}
-        onChangeText={onChangeText}
-        value={text}
-      />
-        <View>
 
+      <Animated.View style={[styles.profileview, {flex: flexAnim}]}>
+        {!keyboardVisible && <Userdata month="03" year="23" />}
+      </Animated.View>
+
+      <View style={styles.listpaiement}>
+        <View style={styles.v_view}>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeNumber}
+            value={number}
+            placeholder="price..."
+            placeholderTextColor={greyColor}
+            keyboardType="numeric"
+          />
         </View>
+
+        <Paimentprice />
+
+        <TouchableOpacity></TouchableOpacity>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
